fd8c2768d21b7b74b2f0a4d5bd6a35c04236ea2f
savethetunas <74320483+savethetunas@users.noreply.github.com>
2022-04-21 12:37:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ switcher.addEventListener("click", function () {
 
   var className = document.body.className;
   if (className == "light-theme") {
-    this.textContent = "Cick to change if u like it dark";
+    this.textContent = "Dark";
   } else {
-    this.textContent = "Cick to change if u like it dark";
+    this.textContent = "Dark";
   }
 });
