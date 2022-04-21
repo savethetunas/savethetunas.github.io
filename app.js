322c34128d9838351778e8fcea6d8f89b2a53a79
savethetunas <74320483+savethetunas@users.noreply.github.com>
2022-04-21 12:45:25 -0700

    as

--- app.js
+++ app.js
@@ -4,11 +4,4 @@ const switcher = document.querySelector(".btn");
 
 switcher.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
-
-  var className = document.body.className;
-  if (className == "light-theme") {
-    this.textContent = "Mode";
-  } else {
-    this.textContent = "Mode";
-  }
 });
